6cb005cb6ef7050a99a9087fa5dc5a7ccc228feb
K <JuanEscobarGT@outlook.com>
2024-03-20 00:05:12 -0600

    [last-class] 35 Filas

--- app/src/main/java/com/example/primeraapp/MainActivity.kt
+++ app/src/main/java/com/example/primeraapp/MainActivity.kt
@@ -5,9 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -21,7 +33,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Content()
-            Texto()
         }
     }
 }
@@ -40,8 +51,34 @@ class MainActivity : ComponentActivity() {
 @Composable
 @Preview(showBackground = true)
 fun Content() {
+   Column (modifier = Modifier
+       .fillMaxSize()
+       .wrapContentSize(Alignment.Center)
+       .padding(horizontal = 5.dp)
+   ) {
+       Texto(texto = "Bienvenido")
+       Space()
+       Texto(texto = "A Jetpack")
+       Space()
+       Texto(texto = "Compose")
+       Space()
+       Row(modifier = Modifier
+           .fillMaxWidth(),
+           horizontalArrangement = Arrangement.SpaceEvenly
+       ) {
+           Circulo(color = Color.Red)
+           Circulo(color = Color.Blue)
+           Circulo(color = Color.Black)
+           Circulo(color = Color.Green)
+       }
+   }
+}
+
+@Composable
+fun Texto(texto: String)
+{
     Text(
-        text = "Hola jetpack",
+        text = texto,
         color = Color.Red,
         fontWeight = FontWeight.Bold,
         fontSize = 40.sp,
@@ -49,22 +86,21 @@ fun Content() {
         modifier = Modifier
             .background(Color.Black)
             .padding(horizontal = 30.dp)
+            .fillMaxWidth()
             .clickable { println("Hola Jetpack") }
     )
 }
 
 @Composable
-fun Texto()
-{
-    Text(
-        text = "Hola jetpack",
-        color = Color.Red,
-        fontWeight = FontWeight.Bold,
-        fontSize = 40.sp,
-        textAlign = TextAlign.Center,
-        modifier = Modifier
-            .background(Color.Black)
-            .padding(horizontal = 30.dp)
-            .clickable { println("Hola Jetpack") }
+fun Circulo(color: Color) {
+    Box(modifier = Modifier
+        .size(70.dp)
+        .background(color, CircleShape)
     )
+}
+
+@Composable
+fun Space()
+{
+    Spacer(modifier = Modifier.height(5.dp))
 }
